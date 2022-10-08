e4b36555680f8e7561fbb81070783bc8d2cac9b4
Olga Kudryavtseva <105752494+Encephalon1@users.noreply.github.com>
2022-10-08 14:42:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import time
+from playsound import playsound
+
+
+def gettime():
+    users_time = input('Введите время для таймера в формате "часы:минуты:секунды": ')
+    hour_min_sec = list(map(int, users_time.split(':')))
+    sec = hour_min_sec[0]*3600 + hour_min_sec[1]*60 + hour_min_sec[2]
+    return sec
+
+
+def timer():
+    sec = gettime()
+    time.sleep(sec)
+    playsound('D:/Matrix/z/Timer/Ludwig van Beethoven - Allegretto.mp3')
+
+
+timer()
