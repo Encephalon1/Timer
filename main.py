fc34451875e4cdfeb49341d100c2edfa16dcd9e0
Olga Kudryavtseva <105752494+Encephalon1@users.noreply.github.com>
2022-10-08 14:46:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from playsound import playsound
 def gettime():
     users_time = input('Введите время для таймера в формате "часы:минуты:секунды": ')
     hour_min_sec = list(map(int, users_time.split(':')))
-    sec = hour_min_sec[0]*3600 + hour_min_sec[1]*60 + hour_min_sec[2]
+    sec = hour_min_sec[0]*3600 + hour_min_sec[1]*60 + hour_min_sec[2] - 3
     return sec
 
 
